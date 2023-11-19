87a6a075bac9fc401ef5a00c3c926162557dd852
mbiraquit <113325925+mbiraquit@users.noreply.github.com>
2023-11-19 19:49:27 +0800

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,42 @@
+	let header = document.querySelector('header');
+    let menu = document.querySelector('#menu-icon');
+    let navbar = document.querySelector('.navbar');
+     
+     
+    window.addEventListener('scroll', () => {
+        header.classList.toggle('shadow', window.scrollY > 0);
+    });
+     
+    menu.onclick = () => {
+        navbar.classList.toggle('active');
+    }
+    window.onscroll = () => {
+        navbar.classList.remove('active');
+    }
+    function reveal() {
+        var reveals = document.querySelectorAll(".reveal");
+      
+        for (var i = 0; i < reveals.length; i++) {
+          var windowHeight = window.innerHeight;
+          var elementTop = reveals[i].getBoundingClientRect().top;
+          var elementVisible = 150;
+      
+          if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+          } else {
+            reveals[i].classList.remove("active");
+          }
+        }
+      }
+      
+      window.addEventListener("scroll", reveal);
+
+       // typing kineme
+    var typed = new Typed(".typing", {
+      strings: ["LEXTER VON"],
+      typeSpeed: 200,
+      backSpeed: 30,
+      loop: true
+  });
+
+  
